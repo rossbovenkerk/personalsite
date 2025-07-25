a670b21299495a5dbbff5a415346c781d5bd778c
unknown <rossbovenkerk@gmail.com>
2025-07-25 09:53:31 -0400

    Clean up desktop view

--- src/app/windows.ts
+++ src/app/windows.ts
@@ -2,10 +2,10 @@ import { Window } from './window';
 
 export const WINDOWS: Window[] = [
     { id: 1, type: 'doc',  title: 'Resume.doc', content: 'xxx' },
-    { id: 2, type: 'link', title: 'LinkedIn', content: 'yyy' },
+    { id: 2, type: 'link', title: 'YouTube', content: 'yyy' },
     { id: 3, type: 'notepad', title: 'Notepad', content: 'zzz' },
 
-    { id: 4, type: 'link', title: 'LinkedIn', content: 'yyy' },
+    /*{ id: 4, type: 'link', title: 'LinkedIn', content: 'yyy' },*/
     { id: 5, type: 'kazaa', title: 'Kazaa', content: 'yyy' },
  /*   { id: 6, type: 'link', title: 'LinkedIn', content: 'yyy' },
     { id: 7, type: 'link', title: 'LinkedIn', content: 'yyy' },
